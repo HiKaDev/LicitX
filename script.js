bd6730c7a2fd7b6e43ee98879abf8344b71bdbb7
HiKaDev <karen.vasconcelos@contatoserv.com.br>
2024-10-21 08:24:33 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function saudacao() {
+    alert('Bem vindo!! Ao novo jeito de licitar!.');
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('JavaScript está funcionando corretamente!');
+});
